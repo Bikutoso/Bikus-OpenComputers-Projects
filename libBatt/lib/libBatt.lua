879a6c1682c71bb8f93f7297977ac11f6ce78d04
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2023-10-07 15:31:37 +0200

    Add setPrimary

--- libBatt/lib/libBatt.lua
+++ libBatt/lib/libBatt.lua
@@ -3,11 +3,12 @@ local component = require("component")
 
 battery.list = {}
 battery.address = nil
+local manualAddress = false
 
 function battery.refresh()
   -- Reset
   battery.list = {}
-  battery.address = nil
+  if not manualAddress then battery.address = nil end
   
   local devices = component.list()
   for addr, _ in pairs(devices) do
@@ -25,6 +26,17 @@ function battery.refresh()
   return battery.address
 end
 
+function battery.setPrimary(addr)
+  if battery.list[addr] ~= nil then
+    battery.address = addr
+    manualAddress = true
+  elseif addr == nil then
+    --Doesn't set a new address but allow a new one to be slected on refresh
+    manualAddress = false
+  end
+  return battery.address
+end
+
 function battery.getEnergyStored()
   -- TODO
   return 0
@@ -34,4 +46,5 @@ function battery.getMaxEnergyStored()
   return 0
 end
 
+battery.refresh()
 return battery
