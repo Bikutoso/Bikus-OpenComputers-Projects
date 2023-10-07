cc55d34f7c49250ae2d96a855bc1d8c38461373b
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2023-10-07 17:37:53 +0200

    implemented getEnergyStored and getMaxEnergyStored

--- libBatt/lib/libBatt.lua
+++ libBatt/lib/libBatt.lua
@@ -5,24 +5,62 @@ battery.list = {}
 battery.address = nil
 local manualAddress = false
 
+local getEnergyStoredMethods = {
+  RF = "getEnergyStored",
+  EU = "getEnergy"
+}
+local getMaxEnergyStoredMethods = {
+  RF = "getMaxEnergyStored",
+  EU = "getCapacity"
+}
+
+local function selectBattery(addr)
+  --If address is slected use that, otherwise select the default
+  local manAddr = true
+  if addr == nil then
+    addr = battery.address
+    manAddr = false
+  end
+  
+  local proxy = component.proxy(addr)
+  --Components could be out of date refresh
+  if not manAddr and proxy == nil then
+    battery.refresh()
+    proxy = component.proxy(addr)
+  elseif manAddr and proxy == nil then
+    error("Invalid address: "..addr)
+  end
+  
+  return proxy, addr
+end
+
 function battery.refresh()
   -- Reset
   battery.list = {}
-  if not manualAddress then battery.address = nil end
+  if manualAddress and component.proxy(battery.address) == nil then
+      manualAddress = false
+  end
   
+  if not manualAddress then
+    battery.address = nil
+  end
+
   local devices = component.list()
   for addr, _ in pairs(devices) do
     devicemethods = component.methods(addr)
 
     if devicemethods["getEnergyStored"] ~= nil then
       battery.list[addr] = "RF"
-      if battery.address == nil then battery.address = addr end
     elseif devicemethods["getEnergy"] ~= nil then
       battery.list[addr] = "EU"
-      if battery.address == nil then battery.address = addr end
     end
   end
 
+  if battery.address == nil then
+    battery.address, _ = next(battery.list)
+    if battery.address == nil then error("No batteries connected") end
+  end
+  
   return battery.address
 end
 
@@ -34,16 +72,18 @@ function battery.setPrimary(addr)
     --Doesn't set a new address but allow a new one to be slected on refresh
     manualAddress = false
   end
-  return battery.address
+  return battery.address, addr
 end
 
-function battery.getEnergyStored()
-  -- TODO
-  return 0
+function battery.getEnergyStored(addr)
+  local proxy, addr = selectBattery(addr)
+  local power = proxy[getEnergyStoredMethods[battery.list[addr]]]()
+  return power
 end
-function battery.getMaxEnergyStored()
-  -- TODO
-  return 0
+function battery.getMaxEnergyStored(addr)
+  local proxy, addr = selectBattery(addr)
+  local power = proxy[getMaxEnergyStoredMethods[battery.list[addr]]]()
+  return power
 end
 
 battery.refresh()
