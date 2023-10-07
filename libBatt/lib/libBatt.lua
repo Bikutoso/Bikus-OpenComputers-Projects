08083709be613c640bf1d782eede80fc8ade1d6a
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2023-10-07 19:55:21 +0200

    man and programs.cfg

--- libBatt/lib/libBatt.lua
+++ libBatt/lib/libBatt.lua
@@ -1,8 +1,8 @@
 local battery  = {}
 local component = require("component")
 
-battery.list = {}
-battery.address = nil
+local battery.list = {}
+local battery.address = nil
 local manualAddress = false
 local convertPowerType = nil
 
@@ -45,6 +45,14 @@ local function convertPower(addr, power)
   end
 
   error("Unable to convert to type: "..convertPowerType)
+end
+
+function list()
+  return battery.list
+end
+
+function address()
+  return battery.address
 end
 
 function battery.refresh()
