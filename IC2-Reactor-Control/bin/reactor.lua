1328c2e0be6a8ab28cfcd5f19d70258fb248d8d3
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2023-10-10 05:08:59 +0200

    renamed functions and moved stuff around

--- IC2-Reactor-Control/bin/reactor.lua
+++ IC2-Reactor-Control/bin/reactor.lua
@@ -60,6 +60,7 @@ battery.high = battery.battery.getMaxEnergyStored() * env.batteryUpperLimit
 battery.low = battery.battery.getMaxEnergyStored() * env.batteryLowerLimit
 battery.battery.convert("RF")
 
+-- Screen related functions
 local function setColor(fore, back)
   gpu.setForeground(fore)
   gpu.setBackground(back)
@@ -69,14 +70,14 @@ local function initScreen ()
   gpu.fill(1, 1, termWidth, termHeight, " ")
   setColor(0xFFFFFF, 0x000000)
   gpu.set(1,2, "IC2 Reactor Control (by Bikutoso)")
-  
+
   gpu.set(1,3, string.rep("=", termWidth))
   gpu.set(1,4, "Status:  Unknown")
   gpu.set(1,5, "Safety:  Unknown")
 
   gpu.set(1,7, "Reactor: 0/0 (Limit: 0)")
   gpu.set(10,8, "0 EU/t")
-  
+
   gpu.set(1,10, "Battery: 0/0 (High: 0, Low: 0)")
 end
 
@@ -113,55 +114,21 @@ local function updateScreen()
   end
   gpu.set(10,5, safety)
   setColor(0xFFFFFF, 0x000000)
-  
+
   local heat = misc.format_thousand(reactor.heat.current).."/"..misc.format_thousand(reactor.heat.max)
   .." (Limit: "..misc.format_thousand(reactor.heat.limit)..")"
   gpu.set(10,7, heat)
-  
+
   local output = misc.format_thousand(reactor.output).." RF/t"
   gpu.set(10,8, output)
-  
+
   local bat = misc.format_thousand(battery.current).."/"..misc.format_thousand(battery.max)
   .." RF (High: "..misc.format_thousand(battery.high)..",  Low: "..misc.format_thousand(battery.low)..")"
   gpu.set(10,10, bat)
 end
 
-local function RecError(msg)
-  setReactorOutput(0)
-  reactor.state = "Critical"
-  
-  setColor(0xFFFF00, 0xFF0000)
-  print("===CRITICAL===")
-  print("Reactor Shutdown!!")
-  print("Reason: "..msg)
-
-  os.exit(1)
-end
-
-local function increaseProtect()
-  reactor.protect.count = reactor.protect.count + 1
-  
-  if reactor.protect.count >= env.reactorOverheatMaxCount then
-    RecError("Abnormal temperature. Shutdown down for safety")
-  end
-end
-
-local function checkHeat()
-  if reactor.heat.current > reactor.heat.critical then -- Reactor Hard Limit
-    RecError("Reactor temperature "..reactor.heat.current.."/"..reactor.heat.max)
-  elseif reactor.state == "Running" and reactor.heat.current > reactor.heat.limit then -- Reactor Soft Limit
-    setReactorOutput(0)
-    reactor.tick = false
-    reactor.state = "Hot"
-    -- Overheat Protection (if enabled)
-    if env.reactorOverheatProtection then increaseProtect() end
-  elseif status == "Hot" and reactor.heat.current < reactor.heat.low then
-    reactor.state = "Running"
-  end
-
-end
-
-local function checkBatt()
+-- Battery related functions
+local function batteryLevelCheck()
   if reactor.state == "Running" and battery.current > battery.high then -- Battery High
     reactor.state = "Batt"
   elseif reactor.state == "Batt" and battery.current < battery.low then -- Battery Low
@@ -169,13 +136,50 @@ local function checkBatt()
   end
 end
 
-local function setReactorOutput(value)
+-- Reactor related functions
+
+local function reactorOutput(value)
   reactor.redstone.setOutput(sides[env.redstoneSide], value)
 end
 
-local function tickReactor()
+local function reactorTick()
   reactor.tick = not reactor.tick
-  setReactorOutput(reactor.tick and 15 or 0)
+  reactorOutput(reactor.tick and 15 or 0)
+end
+
+local function criticalError(msg)
+  reactorOutput(0)
+  reactor.state = "Critical"
+
+  setColor(0xFFFF00, 0xFF0000)
+  gpu.fill(1, 1, termWidth, termHeight, " ")
+  print("===CRITICAL===")
+  print("Reactor Shutdown!!")
+  print("Reason: "..msg)
+
+  os.exit(1)
+end
+
+local function reactorHeatProtect()
+  reactor.protect.count = reactor.protect.count + 1
+
+  if reactor.protect.count >= env.reactorOverheatMaxCount then
+    criticalError("Abnormal temperature. Shutdown down for safety")
+  end
+end
+
+local function reactorHeatCheck()
+  if reactor.heat.current > reactor.heat.critical then -- Reactor Hard Limit
+    criticalError("Reactor temperature "..reactor.heat.current.."/"..reactor.heat.max)
+  elseif reactor.state == "Running" and reactor.heat.current > reactor.heat.limit then -- Reactor Soft Limit
+    reactorOutput(0)
+    reactor.tick = false
+    reactor.state = "Hot"
+    -- Overheat Protection (if enabled)
+    if env.reactorOverheatProtection then reactorHeatProtect() end
+  elseif reactor.state == "Hot" and reactor.heat.current < reactor.heat.low then
+    reactor.state = "Running"
+  end
 end
 
 -- Final stuff before main loop
@@ -186,11 +190,11 @@ while true do
   battery.current = battery.battery.getEnergyStored()
   reactor.output = reactor.reactor.getReactorEUOutput() * 4 -- to RF
 
-  checkHeat()
-  checkBatt()
- 
+  reactorHeatCheck()
+  batteryLevelCheck()
+
   if reactor.state == "Running" then
-    tickReactor()
+    reactorTick()
   end
 
   updateScreen()
