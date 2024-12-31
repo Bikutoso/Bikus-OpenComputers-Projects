48deb1120dc359702d20951d882c1abbe65e4d56
Victoria Solli <10584311+Bikutoso@users.noreply.github.com>
2024-12-31 18:56:22 +0100

    Improved styling

--- libBatt/lib/libbatt.lua
+++ libBatt/lib/libbatt.lua
@@ -5,13 +5,13 @@ local battery  = {}
 
 -- Variables
 list = {}
-address = nil
-local manualAddress = false
-local PowerType = nil
-local PowerRatio = 4
+local currentAddress = nil
+local currentPowerType = nil
+local currentPowerRatio = 4
+local isAddressManual = false
 
 -- Power Name, Unit, StoredPower, MaxPower
-local PowerTypes = {
+local availablePowerTypes = {
   ["Redstone Flux"] = {"RF", "getEnergyStored", "getMaxEnergyStored"},
   ["GregTech 5 EU"] = {"EU", "getEUStored", "getEUMaxStored"},
   ["GregTech CEu EU"] = {"EU", "getEnergyStored", "getEnergyCapacity"},
@@ -22,149 +22,147 @@ local PowerTypes = {
 -- Interal Functions
 -- =================
 
-local function convertPower(addr, power)
-  -- TODO: Make ratio user configurable, since CEu supports user set ones
-  pType = battery.getUnit(addr)
-  if PowerType == "EU" and battery.list[addr][1] == "RF" then
-    return power / PowerRatio
-  elseif PowerType == "RF" and battery.list[addr][1] == "EU" then
-    return power * PowerRatio
+local function convert(address, power)
+  if currentPowerType == "EU" and battery.list.address[1] == "RF" then
+    return power / currentPowerRatio
+  elseif currentPowerType == "RF" and battery.list.address[1] == "EU" then
+    return power * currentPowerRatio
   end
 
   return power
 end
 
-local function isType(addr, type)
-  if battery.getUnit(addr) == type then
-    print("true")
-    return true
-  end
-  print("false")
-  return false
-end
-
 -- HACK: With how stupid GT5 handles battery buffers count each battery
-local function getBattPower(addr, method)
-  local curCount = 0
-  local newCount = 0
-  for iBatt = 1, 16, 1 do
-    newCount = component.invoke(addr, method, iBatt)
-    if newCount == nil then
-      break
-    end
+local function getGtBatteryPower(address, method)
+  local totalPower = 0
 
-    curCount = curCount + newCount
+  for selectedBattery = 1, 16, 1 do
+    local addPower = component.invoke(address, method, selectedBattery
+
+    if addPower == nil then break end
+    totalPower = totalPower + addPower
   end
-  return curCount
+  
+  return totalPower
 end
 
 -- Internal Module Functions
 
-function battery._selectBattery(addr)
-  --If address is slected use that, otherwise select the default
-  local manAddr = true
-  if addr == nil then
-    addr = battery.address
-    manAddr = false
+function battery._selectBattery(address)
+  -- Use address if specified
+  local isAddressLocked = address and true or false
+
+  if not isAddressLocked and address == nil then
+    address = battery.currentAddress
   end
-  
+
   local proxy = component.proxy(addr)
-  --Components could be out of date refresh
-  if not manAddr and proxy == nil then
-    addr = battery.refresh()
-    proxy = component.proxy(addr)
-  elseif proxy == nil then
-    error("Invalid address: "..addr)
-  end
   
-  return proxy, addr
+  --Components could be out of date refresh
+  if not isAddressLocked and proxy == nil then
+    address = battery.refresh()
+    proxy = component.proxy(address)
+  elseif proxy == nil then
+    error("Invalid address: " .. address)
+  end
+
+  return proxy, address
+end
+
+local function isType(addr, type)
+  return battery.getUnit(addr) == type and true or false
 end
 
 -- Universal Functions
 -- ===================
 
 function battery.refresh()
-  -- Reset
-  battery.list = {}
-  if manualAddress and component.proxy(battery.address) == nil then
-      manualAddress = false
-  end
+  battery.list = {} -- Reset device list
   
-  if not manualAddress then
-    battery.address = nil
+  if isAddressManual and component.proxy(battery.currentAddress) == nil then
+      isAddressManual = false
+  end
+
+  if not isAddressManual then
+    battery.currentAddress = nil
   end
 
   local devices = component.list()
-  for addr, _ in pairs(devices) do
-    devicemethods = component.methods(addr)
-    for _, pType in pairs(PowerTypes) do
-      if devicemethods[pType[2]] ~= nil and devicemethods[pType[3]] ~= nil then
-        battery.list[addr] = pType
+  for address, _ in pairs(devices) do
+    availableMethods = component.methods(addr)
+    for _, powerType in pairs(availablePowerTypes) do
+      if availableMethods[powerType[2]] and devicemethods[powerType[3]] then
+        battery.list[address] = powerType
         break
       end
     end
   end
 
-  if battery.address == nil then
-    battery.address, _ = next(battery.list)
-    if battery.address == nil then error("No batteries connected") end
+  if battery.currentAddress == nil then
+    battery.currentAddress, _ = next(battery.list)
+    
+    if battery.currentAddress == nil then
+      error("No batteries connected") -- All devices gone through, no batteries
+    end
   end
 
-  battery.setPowerType(battery.getUnit(battery.address))
-  return battery.address
+  battery.setCurrentPowerType(battery.getUnit(battery.currentAddress))
+  return battery.currentAddress
 end
 
-function battery.setPowerType(type)
-  if type == "RF" then PowerType = "RF"
-  elseif type == "EU" then PowerType = "EU" end
-  return PowerType
+function battery.setCurrentPowerType(powerType)
+  if powerType == "RF" then currentPowerType = "RF"
+  elseif powerType == "EU" then currentPowerType = "EU" end
+
+  return currentPowerType
 end
 
-function battery.setPowerRatio(ratio)
-  local newRatio = tonumber(ratio)
-  
-  if newRatio ~= nil or newRatio ~= 0 then
-    PowerRatio = ratio
-  end
-  return PowerRatio
+function battery.setCurrentPowerRatio(ratio)
+  ratio = tonumber(ratio)
+  if ratio or ratio ~= 0 then currentPowerRatio = ratio end
+
+  return currentPowerRatio
 end
 
-function battery.setPrimary(addr)
-  if battery.list[addr] ~= nil then
-    battery.address = addr
-    manualAddress = true
+function battery.setPrimary(address)
+  if battery.list[address] then
+    battery.currentAddress = address
+    isAddressManual = true
   elseif addr == nil then
-    manualAddress = false
+    isAddressManual = false
     battery.refresh()
   end
-  return battery.address
+  return battery.currentAddress
 end
 
 function battery.getUnit(addr)
-  return battery.list[addr][1]
+  return battery.list.address[1]
 end
 
-function battery.getEnergyStored(addr, side)
-  local proxy, addr = battery._selectBattery(addr)
+function battery.getEnergyStored(address, side)
+  local proxy, address = battery._selectBattery(address)
   local power = 0
+  
   if component.methods(addr)["getBatteryCharge"] == nil then
-    power = proxy[battery.list[addr][2]](side)
+    power = proxy[battery.list.addr[2]](side)
   else
-    power = getBattPower(addr, "getBatteryCharge")
+    power = getGtBatteryPower(address, "getBatteryCharge")
   end
-  return convertPower(addr, power)
+
+  return convert(address, power)
 end
 
-function battery.getMaxEnergyStored(addr, side)
-  local proxy, addr = battery._selectBattery(addr)
+function battery.getMaxEnergyStored(address, side)
+  local proxy, address = battery._selectBattery(addr)
   local power = 0
-  if component.methods(addr)["getMaxBatteryCharge"] == nil then
-    power = proxy[battery.list[addr][3]](side)
+  
+  if component.methods(address)["getMaxBatteryCharge"] == nil then
+    power = proxy[battery.list.address[3]](side)
   else
-    power = getBattPower(addr, "getMaxBatteryCharge")
+    power = getGtBatteryPower(address, "getMaxBatteryCharge")
   end
 
-  return convertPower(addr, power)
+  return convert(address, power)
 end
 
 battery.refresh()
--- libBatt/lib/libbatt_ic2.lua
+++ libBatt/lib/libbatt_ic2.lua
@@ -2,8 +2,8 @@ battery = require("libbatt")
 battery.ic2 = {}
 
 -- TODO: Add guard clause for non IC2 batteries
-function battery.ic2.getSinkTier(addr)
-  local proxy, addr = battery._selectBattery(addr)
+function battery.ic2.getSinkTier(address)
+  local proxy, address = battery._selectBattery(address)
   
   return proxy.getSinkTier()
 end
